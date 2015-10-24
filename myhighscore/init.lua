3db44d8aa7aabf7e3ea9ecb53e73260d6af9dc52
DonBatman <serfdon@gmail.com>
2015-10-24 12:59:20 -0700

    Added formspecs

--- myhighscore/init.lua
+++ myhighscore/init.lua
@@ -1,3 +1,26 @@
+
+
+local button_form = "size[6,8;]"..
+				"background[0,0;6,8;myhighscore_form_bg.png]"..
+				"label[1,0.5;HIGH SCORES]"..
+				"button[1,1;4,1;game;label]"..
+				"button_exit[4,7;1,2;exit;Exit]"
+				
+--place holders
+local game_name = "the game"
+local game_player_name = "the player"
+local game_player_score = "648138"
+
+local game_form = "size[6,8;]"..
+				"background[0,0;6,8;myhighscore_form_bg.png]"..
+				"label[1,0.5;HIGH SCORES FOR "..game_name.."]"..
+				"label[1,1.5;PLAYER]"..
+				"label[3.5,1.5;SCORE]"..
+				"label[0.5,2;"..game_player_name.."]"..
+				"label[3,2;"..game_player_score.."]"..
+				"button[2,7;1,2;back;Back]"..
+				"button_exit[4,7;1,2;exit;Exit]"
+
 minetest.register_node("myhighscore:score_board", {
 	description = "Score Board",
 	tiles = {
@@ -24,7 +47,18 @@ minetest.register_node("myhighscore:score_board", {
 		}
 	},
 
-on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+on_construct = function(pos)
+	local meta = minetest.env:get_meta(pos)
+	meta:set_string("formspec", button_form)
+	meta:set_string("infotext", "High Scores")
+end,
+on_receive_fields = function(pos, formname, fields, sender)
+	local meta = minetest.env:get_meta(pos)
+	if fields['game'] then
+		meta:set_string('formspec', game_form)
+	elseif fields["back"] then
+		meta:set_string('formspec', button_form)
+	end
 end,
 
 })
