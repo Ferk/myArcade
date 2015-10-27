7f5e00299066e9872c21c4999381cf4b3ae7edf2
DonBatman <serfdon@gmail.com>
2015-10-27 09:51:05 -0700

    Added 8 more levels to nibbles

--- nibbles/init.lua
+++ nibbles/init.lua
@@ -1,12 +1,12 @@
 minetest.register_node("nibbles:floor",{
-	description = "Floor",
+	description = "Nibbles Floor",
 	tiles = {"nibbles_floor.png"},
 	paramtype = "light",
 	groups = {cracky = 3, disable_jump = 1, not_in_creative_inventory = 0},
 })
 
 minetest.register_node("nibbles:wall",{
-	description = "Floor",
+	description = "Nibbles Wall",
 	tiles = {
 			"nibbles_wall.png",
 			"nibbles_wall.png",
@@ -24,7 +24,7 @@ minetest.register_node("nibbles:placer",{
 	paramtype = "light",
 	groups = {cracky = 3, not_in_creative_inventory = 0},
 	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-		local schem = minetest.get_modpath("nibbles").."/schems/nibbles.mts"
+		local schem = minetest.get_modpath("nibbles").."/schems/nibbles_level_1.mts"
 		minetest.place_schematic({x=pos.x,y=pos.y-1,z=pos.z},schem,0, "air", true)
 	end,
 })
